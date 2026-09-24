7199fea7cd62eb9a41ae25927324a94526beed9f
arkanws513-spec <arkanws513@gmail.com>
2026-09-24 10:49:48 +0300

    Upgrade city visuals, 3D-style buildings, player art, and smoother camera

--- phaser-street-gangs/src/entities/Player.js
+++ phaser-street-gangs/src/entities/Player.js
@@ -1,1 +1,17 @@
-export class Player extends Phaser.GameObjects.Container{constructor(scene,x,y){super(scene,x,y);this.add([scene.add.circle(0,0,25,0xd9a441,0.12),scene.add.circle(0,0,18,0xd9a441)]);scene.add.existing(this);this.setDepth(30)}}
+export class Player extends Phaser.GameObjects.Container{
+ constructor(scene,x,y){
+  super(scene,x,y);
+  const shadow=scene.add.ellipse(0,24,54,18,0x000000,.35);
+  const glow=scene.add.circle(0,0,31,0xd9a441,.12);
+  const coat=scene.add.polygon(0,0,[[-18,-20],[18,-20],[25,8],[14,25],[-14,25],[-25,8]],0x202a33).setStrokeStyle(2,0xd9a441);
+  const head=scene.add.circle(0,-28,13,0xc99570);
+  const hair=scene.add.arc(0,-31,13,200,340,false,0x15181c);
+  const belt=scene.add.rectangle(0,10,35,6,0x101419);
+  const badge=scene.add.circle(0,-2,5,0xd9a441);
+  const legL=scene.add.rectangle(-9,29,8,18,0x111820);
+  const legR=scene.add.rectangle(9,29,8,18,0x111820);
+  this.add([shadow,glow,coat,head,hair,belt,badge,legL,legR]);
+  scene.add.existing(this);this.setDepth(30);
+  this.setSize(58,76);
+ }
+}
